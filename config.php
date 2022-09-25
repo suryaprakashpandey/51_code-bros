e73a306713834711aafdbebfb91b8eb0596e7e80
Suryaprakash <sppandey299@gmail.com>
2022-09-25 23:18:44 +0530

    Create config.php

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+//  This file contains database configuration assuming you are running mysql using user "root" and password ""
+
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'login');
+
+//  Connecting to the Database
+$conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+//  To check connection
+if($conn == false){
+    dir('Error: Cannot connect');
+}
+
+?>
