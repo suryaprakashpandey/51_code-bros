9cc32bc0e16279d1465d1192d108a698c242d899
Saksham Avasthi <sakshamavasthi7@gmail.com>
2022-09-27 11:37:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -149,3 +149,187 @@
         outer.appendChild(events);
         this.week.appendChild(outer);
     }
+
+    Calendar.prototype.drawEvents = function (day, element) {
+        if (day.month() === this.current.month()) {
+            var todaysEvents = this.events.reduce(function (memo, ev) {
+                if (ev.date.isSame(day, 'day')) {
+                    memo.push(ev);
+                }
+                return memo;
+            }, []);
+
+            todaysEvents.forEach(function (ev) {
+                var evSpan = createElement('span', ev.color);
+                element.appendChild(evSpan);
+            });
+        }
+    }
+
+    Calendar.prototype.getDayClass = function (day) {
+        classes = ['day'];
+        if (day.month() !== this.current.month()) {
+            classes.push('other');
+        } else if (today.isSame(day, 'day')) {
+            classes.push('today');
+        }
+        return classes.join(' ');
+    }
+
+    Calendar.prototype.openDay = function (el) {
+        var details, arrow;
+        var dayNumber = +el.querySelectorAll('.day-number')[0].innerText || +el.querySelectorAll('.day-number')[0].textContent;
+        var day = this.current.clone().date(dayNumber);
+
+        var currentOpened = document.querySelector('.details');
+
+        //Check to see if there is an open detais box on the current row
+        if (currentOpened && currentOpened.parentNode === el.parentNode) {
+            details = currentOpened;
+            arrow = document.querySelector('.arrow');
+        } else {
+            //Close the open events on differnt week row
+            //currentOpened && currentOpened.parentNode.removeChild(currentOpened);
+            if (currentOpened) {
+                currentOpened.addEventListener('webkitAnimationEnd', function () {
+                    currentOpened.parentNode.removeChild(currentOpened);
+                });
+                currentOpened.addEventListener('oanimationend', function () {
+                    currentOpened.parentNode.removeChild(currentOpened);
+                });
+                currentOpened.addEventListener('msAnimationEnd', function () {
+                    currentOpened.parentNode.removeChild(currentOpened);
+                });
+                currentOpened.addEventListener('animationend', function () {
+                    currentOpened.parentNode.removeChild(currentOpened);
+                });
+                currentOpened.className = 'details out';
+            }
+
+            //Create the Details Container
+            details = createElement('div', 'details in');
+
+            //Create the arrow
+            var arrow = createElement('div', 'arrow');
+
+            //Create the event wrapper
+
+            details.appendChild(arrow);
+            el.parentNode.appendChild(details);
+        }
+
+        var todaysEvents = this.events.reduce(function (memo, ev) {
+            if (ev.date.isSame(day, 'day')) {
+                memo.push(ev);
+            }
+            return memo;
+        }, []);
+
+        this.renderEvents(todaysEvents, details);
+
+        arrow.style.left = el.offsetLeft - el.parentNode.offsetLeft + 27 + 'px';
+    }
+
+    Calendar.prototype.renderEvents = function (events, ele) {
+        //Remove any events in the current details element
+        var currentWrapper = ele.querySelector('.events');
+        var wrapper = createElement('div', 'events in' + (currentWrapper ? ' new' : ''));
+
+        events.forEach(function (ev) {
+            var div = createElement('div', 'event');
+            var square = createElement('div', 'event-category ' + ev.color);
+            var span = createElement('span', '', ev.eventName);
+
+            div.appendChild(square);
+            div.appendChild(span);
+            wrapper.appendChild(div);
+        });
+
+        if (!events.length) {
+            var div = createElement('div', 'event empty');
+            var span = createElement('span', '', 'No Events');
+
+            div.appendChild(span);
+            wrapper.appendChild(div);
+        }
+
+        if (currentWrapper) {
+            currentWrapper.className = 'events out';
+            currentWrapper.addEventListener('webkitAnimationEnd', function () {
+                currentWrapper.parentNode.removeChild(currentWrapper);
+                ele.appendChild(wrapper);
+            });
+            currentWrapper.addEventListener('oanimationend', function () {
+                currentWrapper.parentNode.removeChild(currentWrapper);
+                ele.appendChild(wrapper);
+            });
+            currentWrapper.addEventListener('msAnimationEnd', function () {
+                currentWrapper.parentNode.removeChild(currentWrapper);
+                ele.appendChild(wrapper);
+            });
+            currentWrapper.addEventListener('animationend', function () {
+                currentWrapper.parentNode.removeChild(currentWrapper);
+                ele.appendChild(wrapper);
+            });
+        } else {
+            ele.appendChild(wrapper);
+        }
+    }
+
+    Calendar.prototype.drawLegend = function () {
+        var legend = createElement('div', 'legend');
+        var calendars = this.events.map(function (e) {
+            return e.calendar + '|' + e.color;
+        }).reduce(function (memo, e) {
+            if (memo.indexOf(e) === -1) {
+                memo.push(e);
+            }
+            return memo;
+        }, []).forEach(function (e) {
+            var parts = e.split('|');
+            var entry = createElement('span', 'entry ' + parts[1], parts[0]);
+            legend.appendChild(entry);
+        });
+        this.el.appendChild(legend);
+    }
+
+    Calendar.prototype.nextMonth = function () {
+        this.current.add('months', 1);
+        this.next = true;
+        this.draw();
+    }
+
+    Calendar.prototype.prevMonth = function () {
+        this.current.subtract('months', 1);
+        this.next = false;
+        this.draw();
+    }
+
+    window.Calendar = Calendar;
+
+    function createElement(tagName, className, innerText) {
+        var ele = document.createElement(tagName);
+        if (className) {
+            ele.className = className;
+        }
+        if (innerText) {
+            ele.innderText = ele.textContent = innerText;
+        }
+        return ele;
+    }
+}();
+
+!function () {
+    var data = [
+
+    ];
+
+
+
+    function addDate(ev) {
+
+    }
+
+    var calendar = new Calendar('#calendar', data);
+
+}();
